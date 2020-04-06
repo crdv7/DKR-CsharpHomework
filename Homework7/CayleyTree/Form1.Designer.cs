1150fce3d40ccc63d18d3d2d250d7dc9bb09dd85
CRDVIWNL <15974123760@163.com>
2020-04-06 23:21:33 +0800

    homework7CayleyTree

---
+++ Homework7/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawbutton = new System.Windows.Forms.Button();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.labeln = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDownth2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownth1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownleng = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownN = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownper1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownper2 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.drawpanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownth2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownth1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownleng)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownper1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownper2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawbutton
+            // 
+            this.drawbutton.Location = new System.Drawing.Point(669, 415);
+            this.drawbutton.Name = "drawbutton";
+            this.drawbutton.Size = new System.Drawing.Size(75, 23);
+            this.drawbutton.TabIndex = 0;
+            this.drawbutton.Text = "draw";
+            this.drawbutton.UseVisualStyleBackColor = true;
+            this.drawbutton.Click += new System.EventHandler(this.drawbutton_Click);
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Location = new System.Drawing.Point(146, 338);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(102, 20);
+            this.comboBoxColor.TabIndex = 1;
+            this.comboBoxColor.SelectedIndexChanged += new System.EventHandler(this.comboBoxColor_SelectedIndexChanged);
+            // 
+            // labeln
+            // 
+            this.labeln.AutoSize = true;
+            this.labeln.Location = new System.Drawing.Point(67, 52);
+            this.labeln.Name = "labeln";
+            this.labeln.Size = new System.Drawing.Size(53, 12);
+            this.labeln.TabIndex = 2;
+            this.labeln.Text = "递归深度";
+            this.labeln.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDownth2);
+            this.panel1.Controls.Add(this.numericUpDownth1);
+            this.panel1.Controls.Add(this.numericUpDownleng);
+            this.panel1.Controls.Add(this.numericUpDownN);
+            this.panel1.Controls.Add(this.numericUpDownper1);
+            this.panel1.Controls.Add(this.numericUpDownper2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labeln);
+            this.panel1.Controls.Add(this.comboBoxColor);
+            this.panel1.Location = new System.Drawing.Point(511, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 397);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // numericUpDownth2
+            // 
+            this.numericUpDownth2.Location = new System.Drawing.Point(146, 289);
+            this.numericUpDownth2.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownth2.Name = "numericUpDownth2";
+            this.numericUpDownth2.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownth2.TabIndex = 24;
+            this.numericUpDownth2.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownth2.ValueChanged += new System.EventHandler(this.numericUpDownth2_ValueChanged);
+            // 
+            // numericUpDownth1
+            // 
+            this.numericUpDownth1.Location = new System.Drawing.Point(146, 243);
+            this.numericUpDownth1.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownth1.Name = "numericUpDownth1";
+            this.numericUpDownth1.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownth1.TabIndex = 23;
+            this.numericUpDownth1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownth1.ValueChanged += new System.EventHandler(this.numericUpDownth1_ValueChanged);
+            // 
+            // numericUpDownleng
+            // 
+            this.numericUpDownleng.Location = new System.Drawing.Point(146, 100);
+            this.numericUpDownleng.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDownleng.Minimum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDownleng.Name = "numericUpDownleng";
+            this.numericUpDownleng.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownleng.TabIndex = 22;
+            this.numericUpDownleng.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownleng.ValueChanged += new System.EventHandler(this.numericUpDownleng_ValueChanged);
+            // 
+            // numericUpDownN
+            // 
+            this.numericUpDownN.Location = new System.Drawing.Point(146, 50);
+            this.numericUpDownN.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Name = "numericUpDownN";
+            this.numericUpDownN.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownN.TabIndex = 21;
+            this.numericUpDownN.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownN.ValueChanged += new System.EventHandler(this.numericUpDownN_ValueChanged);
+            // 
+            // numericUpDownper1
+            // 
+            this.numericUpDownper1.DecimalPlaces = 1;
+            this.numericUpDownper1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownper1.Location = new System.Drawing.Point(146, 154);
+            this.numericUpDownper1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownper1.Name = "numericUpDownper1";
+            this.numericUpDownper1.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownper1.TabIndex = 20;
+            this.numericUpDownper1.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            65536});
+            this.numericUpDownper1.ValueChanged += new System.EventHandler(this.numericUpDownper1_ValueChanged);
+            // 
+            // numericUpDownper2
+            // 
+            this.numericUpDownper2.DecimalPlaces = 1;
+            this.numericUpDownper2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownper2.Location = new System.Drawing.Point(146, 195);
+            this.numericUpDownper2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownper2.Name = "numericUpDownper2";
+            this.numericUpDownper2.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDownper2.TabIndex = 19;
+            this.numericUpDownper2.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            65536});
+            this.numericUpDownper2.ValueChanged += new System.EventHandler(this.numericUpDownper2_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(69, 341);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "画笔颜色";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "左分支角度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "右分支角度";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "左分支长度比";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "右分支长度比";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "主干长度";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "请选择绘图的参数";
+            // 
+            // drawpanel
+            // 
+            this.drawpanel.Location = new System.Drawing.Point(45, 12);
+            this.drawpanel.Name = "drawpanel";
+            this.drawpanel.Size = new System.Drawing.Size(431, 397);
+            this.drawpanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawpanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.drawbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownth2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownth1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownleng)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownper1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownper2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawbutton;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.Label labeln;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel drawpanel;
+        private System.Windows.Forms.NumericUpDown numericUpDownper1;
+        private System.Windows.Forms.NumericUpDown numericUpDownper2;
+        private System.Windows.Forms.NumericUpDown numericUpDownth2;
+        private System.Windows.Forms.NumericUpDown numericUpDownth1;
+        private System.Windows.Forms.NumericUpDown numericUpDownleng;
+        private System.Windows.Forms.NumericUpDown numericUpDownN;
+    }
+}
+
---
+++ Homework7/CayleyTree/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*将课本中例5-31的Cayley树绘图代码进行修改。
+ * 添加一组控件用以调节树的绘制参数。
+ * 参数包括递归深度（n）、主干长度（leng）、右分支长度比（per1）、
+ * 左分支长度比（per2）、右分支角度（th1）、左分支角度（th2）、画笔颜色（pen）。
+*/
+namespace CayleyTree
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 30 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        double leng = 100;
+        int n = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Blue, Name = "蓝色" });
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Red, Name = "红色" });
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Green, Name = "绿色" });
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Yellow, Name = "黄色" });
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Black, Name = "黑色" });
+            comboBoxColor.Items.Add(new ColorName() { Color = Color.Purple, Name = "紫色" });
+            comboBoxColor.SelectedIndex = 0;
+        }
+        class ColorName
+        {
+            public Color Color;
+            public string Name;
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+            private void drawbutton_Click(object sender, EventArgs e)
+        {
+            int x = drawpanel.Width / 2;
+            if (graphics == null) graphics = drawpanel.CreateGraphics();
+            else graphics.Clear(Color.White);
+            DrawCayleytree(n, x, 310, leng, -Math.PI / 2);
+        }
+        void DrawCayleytree(int n,double x0,double y0,double leng,double th) {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            DrawLine(x0, y0, x1, y1);
+            DrawCayleytree(n - 1, x1, y1, per1 * leng, th + th1);
+            DrawCayleytree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void DrawLine(double x0,double y0,double x1,double y1)
+        {
+            ColorName colorName = (ColorName)comboBoxColor.SelectedItem;
+            Pen pen = new Pen(colorName.Color);
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void hScrollBarper1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void numericUpDownleng_ValueChanged(object sender, EventArgs e)
+        {
+            leng = (double)numericUpDownleng.Value;
+        }
+
+        private void numericUpDownper2_ValueChanged(object sender, EventArgs e)
+        {
+            per2 = (double)numericUpDownper2.Value;
+        }
+
+        private void numericUpDownth1_ValueChanged(object sender, EventArgs e)
+        {
+            th1 = (double)numericUpDownth1.Value * Math.PI / 180;
+        }
+
+        private void numericUpDownth2_ValueChanged(object sender, EventArgs e)
+        {
+            th2 = (double)numericUpDownth2.Value * Math.PI / 180;
+        }
+
+        private void numericUpDownper1_ValueChanged(object sender, EventArgs e)
+        {
+            per1 = (double)numericUpDownper1.Value;
+        }
+
+        private void numericUpDownN_ValueChanged(object sender, EventArgs e)
+        {
+            n = (int)numericUpDownN.Value;
+        }
+
+        private void comboBoxColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
